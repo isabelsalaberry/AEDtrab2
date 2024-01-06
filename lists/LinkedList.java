97c8ea35ba12dea535f9bc3ddb3c1c84263a73b7
Pedro Henrique Filla Decolin <a52982@alunos.ipb.pt>
2024-01-06 16:44:34 +0000

    methods implements

--- lists/LinkedList.java
+++ lists/LinkedList.java
@@ -1,4 +1,70 @@
 package lists;
 
-public class LinkedList {
+import java.util.Iterator;
+
+public class LinkedList<E> implements List<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public boolean add(int i, E e) {
+        return false;
+    }
+
+    @Override
+    public E set(int i, E e) {
+        return null;
+    }
+
+    @Override
+    public void addAll(List<E> outra) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
+
+    @Override
+    public E get(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(E e) {
+        return 0;
+    }
+
+    @Override
+    public E remove(int i) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public LinkedList<E> clone() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
 }
